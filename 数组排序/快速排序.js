2b67c35a0952a81eab1c4a144b071d9d5e6c2c7e
Chiaki-xps <1490295446@qq.com>
2024-11-27 23:52:58 +0800

    快速排序

---
+++ 数组排序/快速排序.js
@@ -0,0 +1,24 @@
+/* 元素交换 */
+function swap(nums, i, j) {
+  let tmp = nums[i];
+  nums[i] = nums[j];
+  nums[j] = tmp;
+}
+
+function partition(nums, left, right) {
+  // 以 nums[left] 为基准数
+  let i = left,
+    j = right;
+  while (i < j) {
+    while (i < j && nums[j] >= nums[left]) {
+      j -= 1; // 从右向左找首个小于基准数的元素
+    }
+    while (i < j && nums[i] <= nums[left]) {
+      i += 1; // 从左向右找首个大于基准数的元素
+    }
+    // 元素交换
+    this.swap(nums, i, j); // 交换这两个元素
+  }
+  this.swap(nums, i, left); // 将基准数交换至两子数组的分界线
+  return i; // 返回基准数的索引
+}
